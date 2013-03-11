7e981821bbf970f1ce528b2360e10912c69ea059
Adam Faris <afaris@linkedin.com>
2013-03-11 15:47:00 -0700

    initial release watch out world

---
+++ lib/Authen/Krb5/Effortless.pm
@@ -0,0 +1,256 @@
+#!/usr/bin/perl
+use 5.10.1;
+use strict;
+use warnings;
+
+
+package Authen::Krb5::Effortless;
+our $VERSION="0.01";
+use Carp;
+use parent qw(Authen::Krb5);
+
+sub new {
+    my $class = shift();
+    my $self  = {};
+    my $a = Authen::Krb5::init_context();                                    # initialize module
+    if ( !$a) { confess( "Unable to initialize Authen::Krb5 module" ); }   
+    bless( $self, $class );
+    return $self;
+}
+
+sub read_cache {
+    my $self = shift();
+    my ( $cc, $cache_pointer, $cache_object );
+    $cc            = Authen::Krb5::cc_default();                                              # load krb5 cache into mem
+    $cache_pointer = $cc->start_seq_get();                                                    # create a cache pointer
+    if ( $cache_pointer ) {
+        $cache_object            = $cc->next_cred( $cache_pointer );    # fetch copy of object from cache
+        $self->{'cache_present'} = 1;                                   # flag to show cache is found
+        $self->{'starttime'}     = $cache_object->starttime();          # start time of credential
+        $self->{'authtime'}      = $cache_object->authtime();
+        $self->{'endtime'}       = $cache_object->endtime();
+        $self->{'principal'}     = $cache_object->client();             # prints principal name
+        $cc->end_seq_get( $cache_pointer );                             # destroy pointer
+    } else {
+        $self->{'cache_present'} = 0;                                   # cache not found
+    }
+    return $self;
+}
+
+sub clear_cache {
+    my $self = shift();                                                    # clear cache if it exists
+    my ( $cc );
+    $cc = Authen::Krb5::cc_default() or confess ("KRB5 Cache error:", Authen::Krb5::error() );                                              
+    if ( $cc ) { 
+        $cc->destroy() or carp ("KRB5 Cache warn: ", Authen::Krb5::error() );
+    } 
+
+    return "True";
+
+}
+
+sub fetch_TGT_KEYTAB {
+    my $self     = shift();
+    my $keytab   = shift();
+    my $username = shift();
+    my ( $TGT, $cc, $principal );
+    $keytab = Authen::Krb5::kt_resolve( $keytab );                              # load keytab into mem
+    if ( !$keytab ) { croak( "KRB5 Keytab error: ", Authen::Krb5::error() ); }
+
+    $cc = Authen::Krb5::cc_default();                                           # initialize default cache
+    if ( !$cc ) { croak( "KRB5 Cache error: ", Authen::Krb5::error() ); }
+
+    $principal = Authen::Krb5::parse_name( $username );                         # initialize principal name
+    if ( !$principal ) { croak( "KRB5 Principal error: ", Authen::Krb5::error() ); }
+
+    $TGT = Authen::Krb5::get_init_creds_keytab( $principal, $keytab );          # Fetch TGT
+    if ( !$TGT ) { croak( "KRB5 TGT error: ", Authen::Krb5::error() ); }
+
+    $cc->initialize( $principal );                                              # store TGT
+    Authen::Krb5::Ccache::store_cred( $cc, $TGT );
+    return "True";
+}
+
+sub fetch_TGT_PW {
+    my $self     = shift();
+    my $pw       = shift();
+    my $username = shift();
+    my ( $TGT, $cc, $principal );
+
+    $cc = Authen::Krb5::cc_default();    # initialize default cache
+    if ( !$cc ) { croak( "KRB5 Cache error: ", Authen::Krb5::error() ); }
+
+    $principal = Authen::Krb5::parse_name( $username );    # initialize principal name
+    if ( !$principal ) { croak( "KRB5 Principal error: ", Authen::Krb5::error() ); }
+
+    $TGT = Authen::Krb5::get_init_creds_password( $principal, $pw );    # Fetch TGT
+    if ( !$TGT ) { croak( "KRB5 TGT error: ", Authen::Krb5::error() ); }
+
+    # store TGT
+    $cc->initialize( $principal );
+    Authen::Krb5::Ccache::store_cred( $cc, $TGT );
+    return "True";
+}
+
+
+return 1;
+
+=pod
+
+=head1 NAME
+
+Authen::Krb5::Effortless - This module is a subclass to Authen::Krb5, adding 'Effortless' ways to authenticate against a KDC server.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+
+=head1 Description
+
+Authen::Krb5::Effortless is more then a 'Simple' module, as it supports both passphrase and keytab based authorization.   
+
+=over 3
+
+=item * 
+
+fetch_TGT_KEYTAB - Will use a keytab file to get a Kerberos credential.  The cache location is usually specified by the shell environment $KRB5CCNAME.
+
+=item * 
+
+fetch_TGT_PW - Will use a password to get and cache a Kerberos credential.  The cache location will be specified by the shell environment $KRB5CCNAME.   
+
+=item * 
+
+clear_cache -  Will clear your local cache of all stored principals.
+
+=item * 
+
+read_cache -  Will read your local cache file and return several values about the cache in a hash. All timestamps are listed in epoch time and are as follows:
+
+=over 6
+
+=item * 
+
+cache_present:   0 or 1
+
+=item * 
+
+starttime:        when the ticket is good from 
+
+=item * 
+
+authtime:         when the authentication occured 
+
+=item * 
+
+endtime:          when the ticket expires 
+
+=item * 
+
+principal:        name of the principal that the cache is valid
+
+=back
+
+
+=back
+
+=head1 EXAMPLES
+
+A keytab example:
+
+  use Authen::Krb5::Effortless;
+  my $username  =  getlogin();                            
+  my $keytab    =  "/path/to/my/keytab";
+  my $krb5      =  KRB5_KeyAuth->new();
+  $krb5->fetch_TGT_KEYTAB($keytab, $username);
+
+
+A password example:
+
+  use Authen::Krb5::Effortless;
+  my $username = getlogin();
+  my $krb5   = KRB5_KeyAuth->new();
+  $krb5->fetch_TGT_PW('sekret_phss_wurd', $username);
+
+
+A example for reading cache:
+
+  use Authen::Krb5::Effortless;
+  my $krb5  = KRB5_KeyAuth->new();
+  $krb5->read_cache();
+  if ($krb5->{'cache_present'}) { 
+    print $krb5->{'principal'}, "\n";
+  }
+
+
+A example for deleting the cache:
+
+  use Authen::Krb5::Effortless;
+  my $krb5  = KRB5_KeyAuth->new();
+  $krb5->clear_cache();
+
+=head1 AUTHOR
+
+Adam Faris, C<< <authen-krb5-effortless at mekanix dot org> >> 
+
+=head1 BUGS
+
+Please report any bugs or feature requests to L<http://github/blah>
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Authen::Krb5::Effortless
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Authen-Krb5-Effortless>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Authen-Krb5-Effortless>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Authen-Krb5-Effortless>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Authen-Krb5-Effortless/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+I would like to acknowledge Jeff Horwitz, Doug MacEachern, Malcolm Beattie, and Scott Hutton 
+for their work on Authen::Krb5.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Adam Faris.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    L<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
